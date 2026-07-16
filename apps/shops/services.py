5ec72e174da75c2c14ab8d9f3ad5a4d123a64278
eliasmarga50-png <eliasmarga50@gmail.com>
2026-07-16 21:09:05 +0300

    feat: add core app service to handle business logic

---
+++ apps/shops/services.py
@@ -0,0 +1,30 @@
+
+
+
+from .models import Shop
+from django.db import transaction
+
+class ShopService:
+	
+	@staticmethod
+	@transaction.atomic
+	def create_shop():
+		
+		
+	
+	@staticmethod
+	@transaction.atomic
+	def update_shop():
+		
+	
+	@staticmethod
+	@transaction.atomic
+	def deactivate_shop(*, shop):
+		
+	
+	
+	@staticmethod
+	@transaction.atomic
+	def activate_shop(*, shop):
+
+
