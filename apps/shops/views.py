d4151dba905ae581e364c184d04eae6837530235
eliasmarga50-png <eliasmarga50@gmail.com>
2026-07-16 22:26:13 +0300

    feat: add views file for http handling of shops

--- apps/shops/views.py
+++ apps/shops/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
 
-# Create your views here.
+
+
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, render, redirect
+from .forms  import ShopCreateForm, ShopUpdateForm
+from .models import Shop
+from .services import ShopService
+
+@login_required
+def shop_list(request):
+	
+	
+@login_required
+def shop_create(request):
+	
+
+@login_required
+def shop_detail(request, slug):
+	
+
+@login_required
+def shop_update(request, slug):
+	
+
+@login_required
+def shop_deactivate(request, slug):
+
+
