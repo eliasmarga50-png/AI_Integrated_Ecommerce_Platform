03eb7e910642c0fbf1e7009186ebe873aa5501cb
eliasmarga50-png <eliasmarga50@gmail.com>
2026-07-16 17:50:28 +0300

    feat: add app file to manage shop configuration

--- apps/shops/apps.py
+++ apps/shops/apps.py
@@ -1,5 +1,27 @@
+
+
+
+from django.apps import AppConfig
+
 from django.apps import AppConfig
 
 
 class ShopsConfig(AppConfig):
-    name = 'shops'
+    """
+    Configuration for the shops application.
+    """
+
+    default_auto_field = "django.db.models.BigAutoField"
+
+    name = "apps.shops"
+
+    verbose_name = "Shops"
+
+    def ready(self):
+        """
+        Application startup configuration.
+        """
+
+        pass
+
+
