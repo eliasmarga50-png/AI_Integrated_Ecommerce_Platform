5f3e8d2ab9ebc8664f81302217b375cfef575516
eliasmarga50-png <eliasmarga50@gmail.com>
2026-07-27 17:48:38 +0300

    mod(admin): add admin interface for reviews

--- apps/reviews/admin.py
+++ apps/reviews/admin.py
@@ -1,7 +1,67 @@
 
 
 
-from django.conf import admin
+from django.contrib import admin
+
 from .models import Review
 
 
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        "product",
+        "user",
+        "rating",
+        "is_edited",
+        "created_at",
+    )
+
+    list_filter = (
+        "rating",
+        "is_edited",
+        "created_at",
+    )
+
+    search_fields = (
+        "title",
+        "comment",
+        "user__username",
+        "product__name",
+    )
+
+    readonly_fields = (
+        "created_at",
+        "updated_at",
+    )
+
+    ordering = ("-created_at",)
+
+    list_per_page = 25
+
+    fieldsets = (
+        (
+            "Review Information",
+            {
+                "fields": (
+                    "product",
+                    "user",
+                    "rating",
+                    "title",
+                    "comment",
+                )
+            },
+        ),
+        (
+            "Metadata",
+            {
+                "fields": (
+                    "is_edited",
+                    "created_at",
+                    "updated_at",
+                )
+            },
+        ),
+    )
+    
+    
+    
