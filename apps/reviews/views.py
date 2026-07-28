c30baad84b6ae9c8e07ebdaf4bbe4b821dc5e75a
eliasmarga50-png <eliasmarga50@gmail.com>
2026-07-28 08:52:53 +0300

    feat(views): add views to handle http request

---
+++ apps/reviews/views.py
@@ -0,0 +1,24 @@
+
+
+
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
+from django.shortcuts import get_object_or_404, render, redirect
+from apps.products.models import Product
+from .forms import ReviewForm
+from .models import Review
+from .services import ReviewService
+
+
+def review_list():
+	
+def review_detail():
+	
+def create_review():
+	
+def update_review():
+	
+def delete_review():
+
+
