1386ff32509b549576e6c3a9ceb7a2b2e6e8b6b8
eliasmarga50-png <eliasmarga50@gmail.com>
2026-07-28 06:25:33 +0300

    feat(services): add business logic to reviews app

---
+++ apps/reviews/services.py
@@ -0,0 +1,22 @@
+
+
+
+
+class ReviewService:
+	
+	@staticmethod
+	def create_review()
+	
+	@staticmethod
+	def update_review()
+	
+	@staticmethod
+	def delete_review()
+	
+	@staticmethod
+	def get_product_review()
+	
+	@staticmethod
+	def calculate_average_rating()
+
+
