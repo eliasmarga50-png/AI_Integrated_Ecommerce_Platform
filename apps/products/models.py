73845bf4b27103b51e1b24b8aaf892b35fb6abe7
eliasmarga50-png <eliasmarga50@gmail.com>
2026-08-08 17:53:53 +0300

    mod(models): add validate price to database

--- apps/products/models.py
+++ apps/products/models.py
@@ -3,6 +3,7 @@
 
 from django.db import models
 from django.utils.text import slugify
+from apps.products.validators import validate_price
 
 
 class Category(models.Model):
@@ -58,45 +59,47 @@ class Product(models.Model):
     category = models.ForeignKey(
         Category,
         on_delete=models.CASCADE,
-        related_name="products"
+        related_name="products",
     )
 
     name = models.CharField(
-        max_length=255
+        max_length=255,
     )
-    
-    image=models.ImageField(
-       upload_to="products/",
-       null=True,
-       blank=True,
+
+    image = models.ImageField(
+        upload_to="products/",
+        null=True,
+        blank=True,
     )
 
     slug = models.SlugField(
         unique=True,
-        blank=True
     )
 
     description = models.TextField()
 
     price = models.DecimalField(
         max_digits=10,
-        decimal_places=2
+        decimal_places=2,
+        validators=[
+            validate_price,
+        ],
     )
 
     stock = models.PositiveIntegerField(
-        default=0
+        default=0,
     )
 
     is_available = models.BooleanField(
-        default=True
+        default=True,
     )
 
     created_at = models.DateTimeField(
-        auto_now_add=True
+        auto_now_add=True,
     )
 
     updated_at = models.DateTimeField(
-        auto_now=True
+        auto_now=True,
     )
 
     class Meta:
@@ -105,6 +108,7 @@ class Product(models.Model):
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.name)
+
         super().save(*args, **kwargs)
 
     def __str__(self):
