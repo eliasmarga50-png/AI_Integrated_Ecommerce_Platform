a151b18bcf5f0c877f800f47eae1ab973f411c0b
eliasmarga50-png <eliasmarga50@gmail.com>
2026-08-14 10:07:03 +0300

    refactor(models): enhance users role to fix dashboard crash with AttributeError

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -52,32 +52,13 @@ class User(AbstractUser):
     )
     
     def is_customer(self):
-    	
-    	"""
-    	Return True if this user has the customer role
-    	"""
-    	return (
-    	    self.is_authenticated and 
-    	    self.role==self.Role.CUSTOMER
-    	)
+    	return self.role==self.Role.CUSTOMER
     	
     def is_seller(self):
-    	"""
-    	Return True if this user has the seller role
-    	"""
-    	return(
-    	    self.is_authenticated and 
-    	    self.role==self.Role.SELLER
-    	)
+    	return self.role==self.Role.SELLER
     	
     def is_admin(self):
-    	"""
-    	Return True if the user has admin role
-    	"""
-    	return (
-    	    self.is_authenticated and
-    	    self.role==self.Role.ADMIN
-    	)
+    	return self.role==self.Role.ADMIN
 
     def __str__(self):
         return self.username
